82f31e876a364c3a7cf47a3ccba7bd4a4ac84671
isotosin <56576124+isotosin@users.noreply.github.com>
2019-10-27 16:34:58 +0100

    Module 3 completed

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,13 +1,19 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+
 public class InfoActivity extends AppCompatActivity {
+
+    TextView address_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +29,24 @@ public class InfoActivity extends AppCompatActivity {
 
     }
 
-    // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
-    // ***
+public void createMapIntent(View v) {
 
-    // ***
+        address_view=(TextView)findViewById(R.id.text_view_address);
+        Uri infoUri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801 ");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, infoUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+    if(mapIntent.resolveActivity(getPackageManager())!=null) {
+
+        startActivity(mapIntent);
+    }
+
+}
+
+
+// ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+
 }
+
+
